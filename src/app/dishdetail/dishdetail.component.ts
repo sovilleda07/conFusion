8bd77a5ff444831edab0210547dc86eb298cd2db
sovilleda07 <sovilleda07@gmail.com>
2020-10-04 22:29:52 -0600

    SPA Part2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,11 @@
-// 2. Importar módulo de entrada "Input"
-import { Component, OnInit, Input } from '@angular/core';
+// 1. Borrar el módulo de entrada "Input"
+import { Component, OnInit } from '@angular/core';
+// 2. Importar Params y Location
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
-// 1. Eliminar constante "DISH"
+// 3. Importar DishService
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -9,12 +13,27 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
-  // 4. Declarar decorador de "Input"
-  @Input()
-  // 3. Declarar variable dish e importar
   dish: Dish;
 
-  constructor() {}
+  // 4. Habilitar servicios y locación
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  // 5. Asignar id del dishdetail
+  ngOnInit(): void {
+    // Tomo el valor que está en el parámetro del URL
+    // tslint:disable-next-line: no-string-literal
+    const id = this.route.snapshot.params['id'];
+
+    // A la variable dish le asignamos el retorno del método getDish
+    this.dish = this.dishService.getDish(id);
+  }
+
+  // 6. Crear método para regresar
+  goBack(): void {
+    this.location.back();
+  }
 }
