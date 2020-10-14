3cc6461ba495dd93a61534cfadaea397195b49f2
sovilleda07 <sovilleda07@gmail.com>
2020-10-13 18:23:27 -0600

    Angular Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,58 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-// 1. Importar para crear formulario
+// 3. Importar "ViewChild" para restablecer form de la plantilla
+// Permitirá obtener acceso a cualquier elemento del DOM de la plantilla.
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// 2. Importar Clase y constante de Feedback
 import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  // 3. Declarar variables:
-  // - Este es el modelo del formulario que va a alojar la forma reactiva.
   feedbackForm: FormGroup;
-  // - Este es el modelo de datos correspondientes.
   feedback: Feedback;
   contactType = ContactType;
+  // 4. Utilizar "@ViewChild" para hacer referencia al FeedbackForm
+  // dándole una variable de plantilla con el nombre "fform"
+  // Esto nos permite obtener acceso al formulario de plantilla para luego restablecerlo
+  @ViewChild('fform') feedbackFormDirective;
 
-  // 4. Inyectar FormBuilder para construir la forma Reactiva dentro del constructor.
   constructor(private fb: FormBuilder) {
-    // 5. Crear método para que cuando se construya la clase, se creará el formulario.
     this.createForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createForm(): void {
+    // 1. Agregar validaciones
+    this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
+      agree: false,
+      contacttype: 'None',
+      message: '',
+    });
   }
 
-  // Crear el Reactive Form y luego se pondrá en la variable "FeedbackForm"
-  createForm(): void {
-    // Para crear el formulario, tomaremos ayuda del "FormBuilder"
-    // con la propiedad "Group" podemos definir un grupo "FormGroup"
-    this.feedbackForm = this.fb.group({
-      // Aquí dentro, vamos a construir las partes de la forma (Form Structure)
-      // y luego mapearlo a la vista
-      firstname: '',
+  onSubmit(): void {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    // 2. Al momento de hacer reset, que regrese a los valores iniciales
+    // Y luego restablecer en la plantilla el formulario también.
+    this.feedbackForm.reset({
+      firtsname: '',
       lastname: '',
       telnum: 0,
       email: '',
       agree: false,
       contacttype: 'None',
-      message:  ''
+      message: '',
     });
-
+    // 5. Restablecer completamente el formulario por medio de la asignación de "@ViewChild"
+    this.feedbackFormDirective.resetForm();
   }
-
-  // 6. Crear método onSubmit
-  onSubmit(): void {
-    // El modelo de formulario es exactamente el mismo que los modelos de datos
-    // por lo que simplemente puedo tomar el valor del modelo de formulario.
-    // Con la propiedad "value" permite recuperar el valor actual de todo el formulario.
-    this.feedback = this.feedbackForm.value;
-    console.log(this.feedback);
-    // Reset el formulario
-    this.feedbackForm.reset();
-  }
-
 }
