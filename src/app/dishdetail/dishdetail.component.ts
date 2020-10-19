1d04947a48dce1b9e67a92a4e640d294a9fb6065
sovilleda07 <sovilleda07@gmail.com>
2020-10-18 19:38:42 -0600

    Angular and RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,8 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+// 3. Importar "SwitchMap"
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -11,6 +13,10 @@ import { DishService } from '../services/dish.service';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  // 1. Declarar variable para almacenar los ID's de los platos
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(
     private dishService: DishService,
@@ -18,12 +24,52 @@ export class DishdetailComponent implements OnInit {
     private location: Location
   ) {}
 
+  // 2. Cambios para obtener el parámetro del URL
   ngOnInit(): void {
-    // tslint:disable-next-line: no-string-literal
-    const id = this.route.snapshot.params['id'];
+    // 2.2
+    // Obtener todos los ids de los platos
+    this.dishService
+      .getDishIds()
+      .subscribe((dishIds) => (this.dishIds = dishIds));
 
-    this.dishService.getDish(id)
-    .subscribe(dish => this.dish = dish);
+    // 2.1
+    // Tener acceso a los parámetros observables
+    // De los parámetros observables obtenemos parámetros que son del tipo Params
+    // Por lo tanto, importamos la biblioteca del enrutador.
+    // Somos capaces de hacer este parámetro de ruta para obtener los parámetros observables
+    // Se utiliza "SwitchMap" en los parámetros observables
+    // Y luego cuando se obtienen los parámetros observables, los tomamos
+    // Y luego llamamos al método.
+    this.route.params
+      .pipe(
+        // tslint:disable-next-line: no-string-literal
+        switchMap((params: Params) => this.dishService.getDish(params['id']))
+      )
+      .subscribe((dish) => {
+        this.dish = dish;
+        this.setPrevNext(dish.id);
+      });
+
+    // Cada vez que los parámetros observables cambian el valor, lo que significa que el
+    // parámetro de ruta cambia el valor, inmediatamente el operador "SwitchMap" tomará
+    // el valor de parámetros, luego hará un "GetDish" desde mi "DishService".
+
+    // Por lo tanto, estamos creando un nuevo observable que es GetDish, que va
+    // a devolver el objeto plato aquí.
+    // Una vez teniendo "GetDish", eso ahora puede estar disponible como observable.
+    // También se susccribe a ese observable, entonces allí se obtiene el plato.
+    // Así que de esta manera, "dish" ahora se actualiza.
+  }
+
+  // 2.3 Creación de métodos para navegar entre los platillo
+  setPrevNext(dishId: string): void {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[
+      (this.dishIds.length + index - 1) % this.dishIds.length
+    ];
+    this.next = this.dishIds[
+      (this.dishIds.length + index + 1) % this.dishIds.length
+    ];
   }
 
   goBack(): void {
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-// 1. Importar rxJS
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
@@ -11,19 +10,20 @@ import { delay } from 'rxjs/operators';
 export class DishService {
   constructor() {}
 
-  // 2. Cambiar return
-  // El método se actualiza para hacer uso de un observable
   getDishes(): Observable<Dish[]> {
     return of(DISHES).pipe(delay(2000));
   }
 
   getDish(id: string): Observable<Dish> {
-    return of(DISHES.filter((dish) => dish.id === id)[0])
-      .pipe(delay(2000));
+    return of(DISHES.filter((dish) => dish.id === id)[0]).pipe(delay(2000));
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return of(DISHES.filter((dish) => dish.featured)[0])
-      .pipe(delay(2000));
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+  }
+
+  // 1. Agregar nuevo método, para devolver todos los ID para todos los platos
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map((dish) => dish.id));
   }
 }
